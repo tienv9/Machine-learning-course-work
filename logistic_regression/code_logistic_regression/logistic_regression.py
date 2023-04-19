3e01c8d110c56f620e24e8dfc5c30c9d92f48823
Eric Vo <48100937+tienv9@users.noreply.github.com>
2023-04-19 00:39:58 -0700

    Logistic regression

---
+++ logistic_regression/code_logistic_regression/logistic_regression.py
@@ -0,0 +1,91 @@
+########## >>>>>> Put your full name and 6-digit EWU ID here. 
+
+# Implementation of the logistic regression with L2 regularization and supports stachastic gradient descent
+
+
+
+
+import numpy as np
+import math
+import sys
+sys.path.append("..")
+
+from code_misc.utils import MyUtils
+
+
+
+class LogisticRegression:
+    def __init__(self):
+        self.w = None
+        self.degree = 1
+
+        
+
+    def fit(self, X, y, lam = 0, eta = 0.01, iterations = 1000, SGD = False, mini_batch_size = 1, degree = 1):
+        ''' Save the passed-in degree of the Z space in `self.degree`. 
+            Compute the fitting weight vector and save it `in self.w`. 
+         
+            Parameters: 
+                X: n x d matrix of samples; every sample has d features, excluding the bias feature. 
+                y: n x 1 vector of lables. Every label is +1 or -1. 
+                lam: the L2 parameter for regularization
+                eta: the learning rate used in gradient descent
+                iterations: the number of iterations used in GD/SGD. Each iteration is one epoch if batch GD is used. 
+                SGD: True - use SGD; False: use batch GD
+                mini_batch_size: the size of each mini batch size, if SGD is True.  
+                degree: the degree of the Z space
+        '''
+
+        # remove the pass statement and fill in the code. 
+
+        pass
+
+    
+    def predict(self, X):
+        ''' parameters:
+                X: n x d matrix; n samples; each has d features, excluding the bias feature. 
+            return: 
+                n x 1 matrix: each row is the probability of each sample being positive. 
+        '''
+    
+        # remove the pass statement and fill in the code. 
+        pass
+    
+    
+    def error(self, X, y):
+        ''' parameters:
+                X: n x d matrix; n samples; each has d features, excluding the bias feature. 
+                y: n x 1 matrix; each row is a labels of +1 or -1.
+            return:
+                The number of misclassified samples. 
+                Every sample whose sigmoid value > 0.5 is given a +1 label; otherwise, a -1 label.
+        '''
+
+        # remove the pass statement and fill in the code.         
+        pass
+    
+    
+
+    def _v_sigmoid(s):
+        '''
+            vectorized sigmoid function
+            
+            s: n x 1 matrix. Each element is real number represents a signal. 
+            return: n x 1 matrix. Each element is the sigmoid function value of the corresponding signal. 
+        '''
+            
+        # Hint: use the np.vectorize API
+
+        # remove the pass statement and fill in the code.         
+        pass
+    
+    
+        
+    def _sigmoid(s):
+        ''' s: a real number
+            return: the sigmoid function value of the input signal s
+        '''
+
+        # remove the pass statement and fill in the code.         
+        pass
+    
